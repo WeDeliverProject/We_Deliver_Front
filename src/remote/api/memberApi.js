bda4855fe9370a294e8d2304920c94f3333f6b2f
jdyj <jdyj@naver.com>
2021-12-12 01:59:00 +0900

    header storage 수정

--- src/remote/api/memberApi.js
+++ src/remote/api/memberApi.js
@@ -3,3 +3,4 @@ import httpClient from "../httpClient";
 
 export const signup = (form) => httpClient.post(`${urls.member}/register`, form);
 export const login = (form) => httpClient.post(`${urls.member}/login`, form);
+export const token = () => httpClient.get(urls.member);
--- src/remote/httpClient.js
+++ src/remote/httpClient.js
@@ -1,8 +1,14 @@
 import axios from "axios";
+import { getDataFromStorage } from "../utils/storage";
 
-const instance = axios.create({
-    baseURL: "http://localhost:3001"
-}
+const token = getDataFromStorage();
+
+const instance = axios.create(
+    {
+        headers: {
+            "Authorization" : token === null ? null : token.accessToken,
+        }
+    }
 )
 
 export default instance;
